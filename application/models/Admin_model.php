e8e9c10c6353738aed61f0b92759bb54674518eb
Hemant katurde <hemantkaturde123@gmail.com>
2023-11-18 13:13:24 +0530

    code for added new changfes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1601,7 +1601,7 @@ class Admin_model extends CI_Model
         //     $data = $query->result_array();
         //     return $data;
         // }else{
-            $this->db->select('*,'.TBL_FINISHED_GOODS.'.net_weight as supplier_goods_net_weight,'.TBL_FINISHED_GOODS.'.sac as supplier_goods_sac,'.TBL_FINISHED_GOODS.'.itemdescription');
+            $this->db->select('*,'.TBL_FINISHED_GOODS.'.net_weight as supplier_goods_net_weight,'.TBL_FINISHED_GOODS.'.sac as supplier_goods_sac,'.TBL_FINISHED_GOODS.'.name as itemdescription');
             // $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
             $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
             $this->db->where(TBL_FINISHED_GOODS.'.status',1);
