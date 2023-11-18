e475d38b8e017fcd65263cf944fd273cd4de0584
Hemant katurde <hemantkaturde123@gmail.com>
2023-11-18 13:09:08 +0530

    code for added new changes

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -2929,40 +2929,40 @@
 			return false;
 		});
 
-		$(document).on('change','.supplier_po_number_for_item',function(e){  
-			e.preventDefault();
-			//$(".loader_ajax").show();
-			var supplier_po_number = $('.supplier_po_number_for_item').val();
+		// $(document).on('change','.supplier_po_number_for_item',function(e){  
+		// 	e.preventDefault();
+		// 	//$(".loader_ajax").show();
+		// 	var supplier_po_number = $('.supplier_po_number_for_item').val();
 
-			var flag = 'Supplier';
+		// 	var flag = 'Supplier';
 
 
-			$("#part_number").html('');
+		// 	$("#part_number").html('');
 		
-			$.ajax({
-				url : "<?php echo ADMIN_PATH;?>getSuppliritemonly",
-				type: "POST",
-				data : {'supplier_po_number' : supplier_po_number,'flag':flag},
-				success: function(data, textStatus, jqXHR)
-				{
-					$(".loader_ajax").hide();
-					if(data == "failure")
-					{
-						$('#part_number').html('<option value="">Select Part Number</option>');
-					}
-					else
-					{
-						$('#part_number').html(data);
+		// 	$.ajax({
+		// 		url : "<?php echo ADMIN_PATH;?>getSuppliritemonly",
+		// 		type: "POST",
+		// 		data : {'supplier_po_number' : supplier_po_number,'flag':flag},
+		// 		success: function(data, textStatus, jqXHR)
+		// 		{
+		// 			$(".loader_ajax").hide();
+		// 			if(data == "failure")
+		// 			{
+		// 				$('#part_number').html('<option value="">Select Part Number</option>');
+		// 			}
+		// 			else
+		// 			{
+		// 				$('#part_number').html(data);
 
-					}
-				},
-				error: function (jqXHR, textStatus, errorThrown)
-				{
-					$('#part_number').html();
-				}
-			});
-			return false;
-		});
+		// 			}
+		// 		},
+		// 		error: function (jqXHR, textStatus, errorThrown)
+		// 		{
+		// 			$('#part_number').html();
+		// 		}
+		// 	});
+		// 	return false;
+		// });
 
 		$(document).on('change','.buyer_po_number_for_item',function(e){  
 			e.preventDefault();
