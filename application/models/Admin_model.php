c7bb1b2152781c81766453dd21c21f368823c72a
Hemant katurde <hemantkaturde123@gmail.com>
2023-11-20 14:57:55 +0530

    code for aaded new chagnwe

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -3570,7 +3570,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
         $this->db->join(TBL_VENDOR_PO_MASTER, TBL_VENDOR_PO_MASTER.'.id = '.TBL_BILL_OF_MATERIAL_ITEM.'.pre_vendor_po_number');
         $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id = '.TBL_BILL_OF_MATERIAL_ITEM.'.pre_buyer_po_number');
-        $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
+        $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
         $this->db->where(TBL_BILL_OF_MATERIAL_ITEM.'.bom_id is NULL');
         $this->db->group_by(TBL_BILL_OF_MATERIAL_ITEM.'.id');
         $query = $this->db->get(TBL_BILL_OF_MATERIAL_ITEM);
