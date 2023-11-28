0bfa17c3dfbb648ad75614873afe05518abb34ca
Hemant katurde <hemantkaturde123@gmail.com>
2023-11-28 08:59:08 +0530

    c0de for added new changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -328,14 +328,7 @@ $route['addrejectionformitemsdatamultientries'] = "admin/addrejectionformitemsda
 $route['saverejectedformitemdata'] = "admin/saverejectedformitemdata";
 $route['viewrejectionformitemdetails'] = "admin/viewrejectionformitemdetails";
 $route['fetch_stock_rejection_form_ttem_details'] = "admin/fetch_stock_rejection_form_ttem_details";
-
 $route['deleterejectionformitem'] = "admin/deleterejectionformitem";
-
-
-
-
-
-
 
 //$route['fetchincomingdeatilsitemlistedit/(:any)'] = "admin/fetchincomingdeatilsitemlistedit/$1";
 
