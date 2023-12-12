55ee2964c488a5f64c3530fa4554255749b7a7be
Hemant katurde <hemantkaturde123@gmail.com>
2023-12-12 18:00:30 +0530

    code for added new chjange

--- application/views/masters/editsupplierMaster.php
+++ application/views/masters/editsupplierMaster.php
@@ -63,7 +63,7 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label for="contact_person">Contact Person <span class="required">*</span></label>
+                                            <label for="contact_person">Contact Person</label>
                                             <input type="text" class="form-control" value="<?=$data['contact_person']?>" id="contact_person" name="contact_person" required>
                                             <p class="error contact_person_error"></p>
                                         </div>
