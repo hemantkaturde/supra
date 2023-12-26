e0f17c43cd836d099155bf3abc927b23154122af
Hemant katurde <hemantkaturde123@gmail.com>
2023-12-26 19:40:28 +0530

    code for added Lot number chnafes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -6011,8 +6011,9 @@ class Admin_model extends CI_Model
     }
 
     public function getItemlistStockform(){
-        $this->db->select('*,'.TBL_STOCKS_ITEM.'.id as stock_item_id');
+        $this->db->select('*,'.TBL_STOCKS_ITEM.'.id as stock_item_id,'.TBL_INCOMING_DETAILS_ITEM.'.lot_number as lot_number_incoming');
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_STOCKS_ITEM.'.part_number');
+        $this->db->join(TBL_INCOMING_DETAILS_ITEM, TBL_INCOMING_DETAILS_ITEM.'.part_number = '.TBL_STOCKS_ITEM.'.part_number');
         $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
         $this->db->where(TBL_STOCKS_ITEM.'.status', 1);
         $this->db->where(TBL_STOCKS_ITEM.'.stock_form_id IS NULL');
