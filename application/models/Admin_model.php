da08c81936b0df5ebe596afca4b3d14c6b3b5965
Hemant katurde <hemantkaturde123@gmail.com>
2024-01-07 13:13:00 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4777,16 +4777,29 @@ class Admin_model extends CI_Model
         // return $data;
 
      
+            // $this->db->select('*,'.TBL_RAWMATERIAL.'.sac as sac_no,'.TBL_SUPPLIER_PO_MASTER_ITEM.'.rate as supplierrate,'.TBL_RAWMATERIAL.'.type_of_raw_material as typeofrawmaterial,'.TBL_SUPPLIER_PO_MASTER_ITEM.'.order_oty as supplier_order_qty');
+            // // $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
+            // $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
+            // $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_SUPPLIER_PO_MASTER_ITEM.'.pre_vendor_name');
+            // $this->db->where(TBL_SUPPLIER_PO_MASTER_ITEM.'.supplier_po_id',$supplier_po_number);
+            // $this->db->where(TBL_RAWMATERIAL.'.status',1);
+            // $this->db->where(TBL_RAWMATERIAL.'.raw_id',$part_number);
+            // $query = $this->db->get(TBL_RAWMATERIAL);
+            // $data = $query->result_array();
+            // return $data;
+
+
             $this->db->select('*,'.TBL_RAWMATERIAL.'.sac as sac_no,'.TBL_SUPPLIER_PO_MASTER_ITEM.'.rate as supplierrate,'.TBL_RAWMATERIAL.'.type_of_raw_material as typeofrawmaterial,'.TBL_SUPPLIER_PO_MASTER_ITEM.'.order_oty as supplier_order_qty');
-            // $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
-            $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
+            $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_RAWMATERIAL.'.raw_id');
+           // $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM, TBL_SUPPLIER_PO_MASTER_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
             $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_SUPPLIER_PO_MASTER_ITEM.'.pre_vendor_name');
             $this->db->where(TBL_SUPPLIER_PO_MASTER_ITEM.'.supplier_po_id',$supplier_po_number);
-            $this->db->where(TBL_RAWMATERIAL.'.status',1);
             $this->db->where(TBL_RAWMATERIAL.'.raw_id',$part_number);
-            $query = $this->db->get(TBL_RAWMATERIAL);
+            $query = $this->db->get(TBL_SUPPLIER_PO_MASTER_ITEM);
             $data = $query->result_array();
             return $data;
+
+
 
     }
 
