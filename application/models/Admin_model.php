19a0b815804b3d14068a9cf38c19ede62f3fa4a4
Hemant katurde <hemantkaturde123@gmail.com>
2024-01-11 17:47:36 +0530

    coded for aade new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -5667,7 +5667,7 @@ class Admin_model extends CI_Model
     public function getTotalDebitAndokQty(){
 
 
-        $this->db->select('sum(debit_amount) as total_debit_amount, sum(SGST_value) as total_SGST_value, sum(CGST_value) as total_CGST_value, sum(IGST_value) as total_IGST_value , sum(total_amount_of_ok_qty_data) as total_amount_of_ok_qty_data,sum(ok_qty) as total_amount_of_ok_qty,sum(p_and_f_charges) as p_and_f_charges');
+        $this->db->select('sum(debit_amount) as total_debit_amount, sum(SGST_value) as total_SGST_value, sum(CGST_value) as total_CGST_value, sum(IGST_value) as total_IGST_value , sum(total_amount_of_ok_qty_data) as total_amount_of_ok_qty_data,sum(total_amount_of_ok_qty) as total_amount_of_ok_qty,sum(p_and_f_charges) as p_and_f_charges');
         $this->db->where(TBL_DEBIT_NOTE_ITEM.'.debit_note_id IS NULL');
         $query = $this->db->get(TBL_DEBIT_NOTE_ITEM);
         $data = $query->result_array();
@@ -5677,7 +5677,7 @@ class Admin_model extends CI_Model
 
     public function getTotalDebitAndokQtyedit($id){
 
-        $this->db->select('sum(debit_amount) as total_debit_amount, sum(SGST_value) as total_SGST_value, sum(CGST_value) as total_CGST_value, sum(IGST_value) as total_IGST_value , sum(total_amount_of_ok_qty_data) as total_amount_of_ok_qty_data,sum(ok_qty) as total_amount_of_ok_qty,sum(p_and_f_charges) as p_and_f_charges');
+        $this->db->select('sum(debit_amount) as total_debit_amount, sum(SGST_value) as total_SGST_value, sum(CGST_value) as total_CGST_value, sum(IGST_value) as total_IGST_value , sum(total_amount_of_ok_qty_data) as total_amount_of_ok_qty_data,sum(total_amount_of_ok_qty) as total_amount_of_ok_qty,sum(p_and_f_charges) as p_and_f_charges');
         $this->db->where(TBL_DEBIT_NOTE_ITEM.'.debit_note_id',$id);
         $query = $this->db->get(TBL_DEBIT_NOTE_ITEM);
         $data = $query->result_array();
