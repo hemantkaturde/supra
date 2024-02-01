3e2b105847b068b932dd28b51e90fc458756f3e2
Hemant katurde <hemantkaturde123@gmail.com>
2024-02-01 10:13:34 +0530

    Changed..Driver Search Functinality

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -1436,7 +1436,7 @@
 					 { "width": "15%", "targets": 4 },
 	                 { "width": "10%", "targets": 5 },
 					 { "width": "10%", "targets": 6 },
-					 { "width": "15%", "targets": 7 },
+					 { "width": "10%", "targets": 7 },
 					 { "width": "15%", "targets": 8 },
 					//  { "width": "15%", "targets": 7 },
 					
