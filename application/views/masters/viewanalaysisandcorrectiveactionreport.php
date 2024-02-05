c533e6eba7da05f362293a35f024a483d2cad624
Hemant katurde <hemantkaturde123@gmail.com>
2024-02-05 23:48:29 +0530

    coce for added new changes

---
+++ application/views/masters/viewanalaysisandcorrectiveactionreport.php
@@ -0,0 +1,55 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            <i class="fa fa-users"></i> Analysis and Corrective Action Report
+            <small>Add,Edit,Delete</small>
+        </h1>
+    </section>
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-6 text-left">
+                <ul class="breadcrumb" style="background-color:#ecf0f5 !important">
+                    <li class="completed"><a href="javascript:void(0);">Masters</a></li>
+                    <li class="active"><a href="javascript:void(0);">Analysis and Corrective Action Report</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 text-right">
+                <div class="form-group">
+                    <a class="btn btn-primary" href="<?php echo base_url(); ?>addanalaysisandcorrectiveactionreport">
+                        <i class="fa fa-plus"></i> Add Analysis and Corrective Action Report</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-body">   
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover" id="view_export_details">
+                                <thead>
+                                    <tr style="background-color:#3c8dbc !important;color:#fff">
+                                        <th>Report No</th>
+                                        <th>Stage</th>
+                                        <th>Date of Observation / Rejected Found</th>
+                                        <th>Drawing No / REV No</th>
+                                        <th>Challan No</th>
+                                        <th>PO NO / WO NO</th>
+                                        <th>Inword No</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+        </div>
+    </section>
+</div>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/common.js" charset="utf-8"></script>
