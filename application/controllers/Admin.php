4f777856296d5fbd75c65e4ac3398d4d6d235fa5
Hemant katurde <hemantkaturde123@gmail.com>
2024-04-15 10:12:59 +0530

    code for added new changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10145,7 +10145,7 @@ public function downlaodsupplierpo($id){
     $item_count =count($getsupplierItemdeatilsForInvoice);
 
     if($item_count==1){
-        $padding_bottom = '120px';
+        $padding_bottom = '80px';
     }else if($item_count==2){
         $padding_bottom = '28px';
     }else if($item_count==3){
