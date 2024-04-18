4d39f2c975835b7dd92adcb10f59a53a382390f4
Hemant katurde <hemantkaturde123@gmail.com>
2024-04-18 19:43:11 +0530

    stock form Series

--- application/views/masters/addNewpackinginstaruction.php
+++ application/views/masters/addNewpackinginstaruction.php
@@ -87,8 +87,6 @@
                                                     $string1 =0;
                                                 }
 
-
-                                               
                                                 $n = 4; // Number of characters to extract from the end
                                                 $lastNCharacters = substr($string1, -$n);
                                                 $inrno= "PI".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
--- application/views/masters/addcreditnote.php
+++ application/views/masters/addcreditnote.php
@@ -26,21 +26,84 @@
                                 <div class="col-md-4">
 
                                     <?php
+
+                                        $current_month = date("n"); // Get the current month without leading zeros
+
+                                        if ($current_month >= 4) {
+                                                // If the current month is April or later, the financial year is from April (current year) to March (next year)
+                                                $financial_year_indian = date("y") . "" . (date("y") + 1);
+                                        } else {
+                                                // If the current month is before April, the financial year is from April (last year) to March (current year)
+                                                $financial_year_indian = (date("y") - 1) . "" . date("y");
+                                        }
+
                                         if($getPreviousCreditnotenumber['credit_note_number']){
                                             // $arr = str_split($getPreviousCreditnotenumber['credit_note_number']);
                                             // $i = end($arr);
                                             // $inrno= "SQCN2324".str_pad((int)$i+1, 4, 0, STR_PAD_LEFT);
                                             // $po_number = $inrno;
 
-                                            $string = $getPreviousCreditnotenumber['credit_note_number'];
-                                            $n = 4; // Number of characters to extract from the end
-                                            $lastNCharacters = substr($string, -$n);
-                                            $inrno= "SQSD2324".str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
-                                            $po_number = $inrno;
+                                            // Old Logic Start Here commit 18-04-2024
+                                            // $string = $getPreviousCreditnotenumber['credit_note_number'];
+                                            // $n = 4; // Number of characters to extract from the end
+                                            // $lastNCharacters = substr($string, -$n);
+                                            // $inrno= "SQSD2324".str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                            // $po_number = $inrno;
+
+
+                                            // New Logic Start Here 
+                                            $getfinancial_year = substr($getPreviousCreditnotenumber['credit_note_number'], -8);
+
+                                            $first_part_of_string = substr($getfinancial_year,0,4);
+                                            $year = substr($getfinancial_year,0,2);
+
+                                            // Current date
+                                            $currentDate = new DateTime();
+                                            
+                                            // Financial year in India starts from April 1st
+                                            $financialYearStart = new DateTime("$year-04-01");
+                                            
+                                            // Financial year in India ends on March 31st of the following year
+                                            $financialYearEnd = new DateTime(($year + 1) . "-03-31");
+                                            
+                                            // Check if the current date falls within the financial year
+                                            if ($currentDate >= $financialYearStart && $currentDate <= $financialYearEnd) {
+                                                
+                                                $string = $getPreviousCreditnotenumber['credit_note_number'];
+                                                $n = 4; // Number of characters to extract from the end
+                                                $lastNCharacters = substr($string, -$n);
+                                                $inrno= "SQCN".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                $po_number = $inrno;
+
+                                            } else {
+
+                                                $string = $getPreviousCreditnotenumber['credit_note_number'];
+                                                $n = 4; // Number of characters to extract from the end
+                                                $lastNCharacters1 = substr($string, -$n);
+
+                                                if($lastNCharacters1  > 0){
+                                                    if ($currentDate >= $financialYearStart && $currentDate <= $financialYearEnd) {
+                                                        $string1 =$getPreviousCreditnotenumber['credit_note_number'];
+                                                    }else{
+                                                        $string1 =0;
+                                                    }                                                   
+                                                }else{
+                                                    $string1 =0;
+                                                }
+
+                                                $n = 4; // Number of characters to extract from the end
+                                                $lastNCharacters = substr($string1, -$n);
+                                                $inrno= "SQCN".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                $po_number = $inrno;
+
+                                                //$po_number = 'SQPO24250001';
+                                            }  
+                                            /* New Logic End Here */
+
 
 
                                         }else{
-                                            $po_number = 'SQCN23240001';
+                                            $po_number = 'SQCN'.$financial_year_indian.'0001';
                                         }
                                     ?>
                                     
