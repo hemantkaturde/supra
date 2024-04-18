a0478b44522e1bc1be5a4b7f83cf54ea7127d8c9
Hemant katurde <hemantkaturde123@gmail.com>
2024-04-18 19:02:52 +0530

    stock form Series

--- application/views/masters/addnewrejectionform.php
+++ application/views/masters/addnewrejectionform.php
@@ -74,7 +74,7 @@
                                                     $n = 4; // Number of characters to extract from the end
                                                     $lastNCharacters = substr($string, -$n);
                                                     $inrno= "SQID".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
-                                                    $incoming_details_id = $inrno;
+                                                    $rejection_number = $inrno;
     
                                                 } else {
     
@@ -82,7 +82,7 @@
                                                     $n = 4; // Number of characters to extract from the end
                                                     $lastNCharacters = substr($string, -$n);
                                                     $inrno= "SQID".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
-                                                    $incoming_details_id = $inrno;
+                                                    $rejection_number = $inrno;
     
                                                     //$po_number = 'SQPO24250001';
                                                 }  
