ced8ad86f858d1e93716f4c6650216d9465d17e1
Hemant katurde <hemantkaturde123@gmail.com>
2024-04-19 12:41:18 +0530

    stock form Series

--- application/views/masters/addjobwork.php
+++ application/views/masters/addjobwork.php
@@ -26,13 +26,80 @@
                                 <div class="col-md-4">
 
                                     <?php
+
+                                        $current_month = date("n"); // Get the current month without leading zeros
+
+                                        if ($current_month >= 4) {
+                                            // If the current month is April or later, the financial year is from April (current year) to March (next year)
+                                            $financial_year_indian = date("y") . "" . (date("y") + 1);
+                                        } else {
+                                            // If the current month is before April, the financial year is from April (last year) to March (current year)
+                                            $financial_year_indian = (date("y") - 1) . "" . date("y");
+                                        }
+
+
                                         if($getPreviousjobworkponumber['po_number']){
-                                            $arr = str_split($getPreviousjobworkponumber['po_number']);
-                                            $i = end($arr);
-                                            $inrno= "SQJW2324".str_pad((int)$i+1, 4, 0, STR_PAD_LEFT);
-                                            $po_number = $inrno;
+                                            // $arr = str_split($getPreviousjobworkponumber['po_number']);
+                                            // $i = end($arr);
+                                            // $inrno= "SQJW2324".str_pad((int)$i+1, 4, 0, STR_PAD_LEFT);
+                                            // $po_number = $inrno;
+
+                                             // New Logic Start Here 
+                                             $getfinancial_year = substr($getPreviousjobworkponumber['po_number'], -8);
+
+                                             $first_part_of_string = substr($getfinancial_year,0,4);
+                                             $year = substr($first_part_of_string,0,2);
+
+                                             // Current date
+                                             $currentDate = new DateTime();
+                                             
+                                             // Financial year in India starts from April 1st
+                                             $financialYearStart = new DateTime("$year-04-01");
+                                             
+                                             // Financial year in India ends on March 31st of the following year
+                                             $financialYearEnd = new DateTime(($year + 1) . "-03-31");
+                                             
+                                             // Check if the current date falls within the financial year
+                                             if ($currentDate >= $financialYearStart && $currentDate <= $financialYearEnd) {
+                                                
+                                                 $string = $getPreviousjobworkponumber['po_number'];
+                                                 $n = 4; // Number of characters to extract from the end
+                                                 $lastNCharacters = substr($string, -$n);
+                                                 $inrno= "SQJW".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                 $po_number = $inrno;
+ 
+                                             } else {
+ 
+                                                   $string = $getPreviousjobworkponumber['po_number'];
+                                                   $n = 4; // Number of characters to extract from the end
+                                                   $lastNCharacters1 = substr($string, -$n);
+                                                   
+                                                   if($lastNCharacters1  > 0){
+
+                                                       if ($currentDate >= $financialYearStart && $currentDate <= $financialYearEnd) {
+
+                                                           $string1 =$getPreviousjobworkponumber['po_number'];
+                                                       }else{
+                                                           $string1 =0;
+                                                       }
+
+                                                   }else{
+                                                       $string1 =0;
+                                                   }
+
+                                                   $lastNCharacters = substr($string1, -$n);
+                                                   $inrno= "SQJW".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                   $po_number = $inrno;
+ 
+                                                 //$po_number = 'SQPO24250001';
+                                             }  
+                                           /* New Logic End Here */
+
+
+
+
                                         }else{
-                                            $po_number = 'SQJW23240001';
+                                            $po_number = 'SQJW'.$financial_year_indian.'0001';
                                         }
                                     ?>
                                     
