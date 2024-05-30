5a054ea29793e9a77af553bd587dfdcd29dfd2c2
Hemant katurde <hemantkaturde123@gmail.com>
2024-05-30 15:45:24 +0530

    code for added neew changes"

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -14665,7 +14665,7 @@ public function downlaoddebitnote($id){
                         <div>
                             <p>Dear Sir,</p>
                             <p><b>Sub: Debit Note</b></p>
-                            <p>With reference to the above subject we have debited your account vide your Inv No. GT-016 Dated 23-Sep-2023. 
+                            <p>With reference to the above subject we have debited your account vide your Inv No.'.$getDebitnoteitemdeatilsForInvoice[0]['invoice_no'].' Dated '.$getDebitnoteitemdeatilsForInvoice[0]['invoice_date'].'. 
                                The details are as follows: </p>
                         <div>    
                     </td>  
