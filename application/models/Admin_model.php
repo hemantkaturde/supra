306d793680008a0362b89bf084738988a52d17b8
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-05 14:15:58 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4823,7 +4823,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id= '.TBL_BILL_OF_MATERIAL.'.vendor_name');
         $this->db->join(TBL_VENDOR_PO_MASTER, TBL_VENDOR_PO_MASTER.'.id= '.TBL_BILL_OF_MATERIAL.'.vendor_po_number');
         $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id= '.TBL_BILL_OF_MATERIAL_ITEM.'.part_number');
-        // $this->db->join(TBL_BILL_OF_MATERIAL_ITEM.' as a', 'a.part_number= '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
+        $this->db->join(TBL_BILL_OF_MATERIAL_ITEM.' as a', 'a.part_number= '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
         $this->db->join(TBL_BUYER_MASTER, TBL_BILL_OF_MATERIAL.'.buyer_name= '.TBL_BUYER_MASTER.'.buyer_id');
         $this->db->join(TBL_FINISHED_GOODS, TBL_BILL_OF_MATERIAL_ITEM.'.part_number= '.TBL_FINISHED_GOODS.'.fin_id');
 
@@ -4846,7 +4846,7 @@ class Admin_model extends CI_Model
         }
 
         $this->db->where(TBL_BILL_OF_MATERIAL.'.status', 1);
-        // $this->db->group_by(TBL_BILL_OF_MATERIAL_ITEM.'.id');
+         $this->db->group_by(TBL_BILL_OF_MATERIAL_ITEM.'.id');
         $this->db->order_by(TBL_BILL_OF_MATERIAL.'.id','DESC');
         $this->db->limit($params['length'],$params['start']);
       
