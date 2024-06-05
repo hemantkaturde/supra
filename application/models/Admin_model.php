fe51dd9ee7f8cf0c87e9c9d3053a774b41d98fb8
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-05 21:53:04 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4834,9 +4834,9 @@ class Admin_model extends CI_Model
 
                             $this->db->join(TBL_BILL_OF_MATERIAL, TBL_BILL_OF_MATERIAL.'.id= '.TBL_BILL_OF_MATERIAL_ITEM.'.bom_id');
                             $this->db->join(TBL_VENDOR_PO_MASTER, TBL_VENDOR_PO_MASTER.'.id= '.TBL_BILL_OF_MATERIAL.'.vendor_po_number');
-                           $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.vendor_po_id= '.TBL_VENDOR_PO_MASTER.'.id');
+                           // $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.vendor_po_id= '.TBL_VENDOR_PO_MASTER.'.id');
 
-                           $this->db->join(TBL_VENDOR_PO_MASTER_ITEM.' as a', 'a.part_number_id= '.TBL_BILL_OF_MATERIAL_ITEM.'.part_number');
+                           //$this->db->join(TBL_VENDOR_PO_MASTER_ITEM.' as a', 'a.part_number_id= '.TBL_BILL_OF_MATERIAL_ITEM.'.part_number');
                          //  $this->db->join(TBL_VENDOR_PO_MASTER, TBL_VENDOR_PO_MASTER.'.id= a.vendor_po_id');
 
         // $this->db->join(TBL_BILL_OF_MATERIAL_ITEM.' as a', 'a.part_number= '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
@@ -4889,7 +4889,7 @@ class Admin_model extends CI_Model
                 $data[$counter]['bom_number'] = $value['v_po_number'];
                 $data[$counter]['date'] = $value['date'];
                 $data[$counter]['fg_part_number'] = $value['partno'];
-                $data[$counter]['vendor_order_qty'] = $value['vendor_order_qty_co'];
+                $data[$counter]['vendor_order_qty'] = '0';
                 $data[$counter]['vendor_received_qty'] = $value['vendor_received_qty_co'];
                 $data[$counter]['buyer_name'] = $value['buyer'];
                
