d9b26d54f07ecf16b5277efb7494f5452ce6ad72
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-07 21:36:34 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4562,7 +4562,7 @@ class Admin_model extends CI_Model
         TBL_BILL_OF_MATERIAL_ITEM.'.vendor_order_qty as vendor_order_qty',
         TBL_BILL_OF_MATERIAL_ITEM.'.vendor_actual_recived_qty as vendor_received_qty',   
         TBL_FINISHED_GOODS.'.net_weight as net_Weight_per_pcs_in_kgs',
-        TBL_BILL_OF_MATERIAL.'.date as vendor_delivery_date',         
+        TBL_VENDOR_PO_MASTER.'.delivery_date as vendor_delivery_date',   
         TBL_BILL_OF_MATERIAL.'.bom_status as bom_status',
         TBL_BILL_OF_MATERIAL_ITEM.'.remark as item_remark',
         TBL_BILL_OF_MATERIAL_ITEM.'.expected_qty as expected_qty',
