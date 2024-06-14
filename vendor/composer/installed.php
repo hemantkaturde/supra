31b261f0c5b24e88b035deda6d3a88b8bf434864
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-14 13:39:27 +0530

    code for added new change

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '189035c6801adc9a0ef52219e8c5c960939ce26f',
+        'reference' => '5ddd697d520bf3a3d2df012aaa0c6e882f0b8e55',
         'name' => 'codeigniter/framework',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '189035c6801adc9a0ef52219e8c5c960939ce26f',
+            'reference' => '5ddd697d520bf3a3d2df012aaa0c6e882f0b8e55',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
