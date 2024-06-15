33d4e5fbcd85da0d201864e5bdb66688bda57fc3
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-15 10:23:23 +0530

    code for added new change

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1674,12 +1674,10 @@ class Admin_model extends CI_Model
 
 
     public function fetchALLFinishgoodListforbuyerpodetailreport(){
-        $this->db->select('*,'.TBL_FINISHED_GOODS.'.fin_id as item_details,'.TBL_BUYER_PO_MASTER_ITEM.'.id as poitemid');
-        $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
-        $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
-        // $this->db->where(TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id',$buyer_po_number);
-        // $this->db->where(TBL_BUYER_PO_MASTER.'.id',$buyer_po_number);
-        $query = $this->db->get(TBL_BUYER_PO_MASTER_ITEM);
+        $this->db->select('*');
+        $this->db->where(TBL_FINISHED_GOODS.'.status', 1);
+        $this->db->order_by(TBL_FINISHED_GOODS.'.part_number', 'ASC');
+        $query = $this->db->get(TBL_FINISHED_GOODS);
         $data = $query->result_array();
         return $data;
     }
@@ -9890,7 +9888,7 @@ class Admin_model extends CI_Model
         }
 
         if($part_number!='NA'){
-            $this->db->where(TBL_BUYER_PO_MASTER_ITEM.'.id', $part_number);
+            $this->db->where(TBL_BUYER_PO_MASTER_ITEM.'.part_number_id', $part_number);
         }
 
         if($from_date!='NA'){
@@ -9941,7 +9939,7 @@ class Admin_model extends CI_Model
         }
 
         if($part_number!='NA'){
-            $this->db->where(TBL_BUYER_PO_MASTER_ITEM.'.id', $part_number);
+            $this->db->where(TBL_BUYER_PO_MASTER_ITEM.'.part_number_id', $part_number);
         }
 
         if($from_date!='NA'){
