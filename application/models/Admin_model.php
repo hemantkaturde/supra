671c05c4a34b0b12aedc81c51cfa43c1fd45db03
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-15 17:21:28 +0530

    code for added new change

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9914,8 +9914,8 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
         $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
         $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_PACKING_INSTRACTION_DETAILS.'.part_number = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
-       // $this->db->join(TBL_PACKING_INSTRACTION, TBL_PACKING_INSTRACTION.'.id = '.TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id');
-        //  $this->db->join(TBL_PACKING_INSTRACTION.' as a', 'a.buyer_po_number = '.TBL_BUYER_PO_MASTER.'.id','left');
+        $this->db->join(TBL_PACKING_INSTRACTION, TBL_PACKING_INSTRACTION.'.id = '.TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id');
+        $this->db->join(TBL_PACKING_INSTRACTION.' as a', 'a.buyer_po_number = '.TBL_BUYER_PO_MASTER.'.id');
 
 
         if($params['search']['value'] != "") 
@@ -9952,7 +9952,7 @@ class Admin_model extends CI_Model
             $this->db->where(TBL_BUYER_PO_MASTER.'.date <=', $todate);
         }
 
-        $this->db->group_by(array('part_number_id','buyer_invoice_number','buyer_po_part_delivery_date'));
+        $this->db->group_by(array('part_number_id','packing_instract_id'));
         $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
         //$this->db->group_by(TBL_BUYER_PO_MASTER.'.sales_order_number,');
 
