fa619447b6720ca6b26ea14bb55fef2d6cc1a7ec
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-27 20:55:33 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9966,7 +9966,7 @@ class Admin_model extends CI_Model
             $todate = $to_date;
             $this->db->where(TBL_BUYER_PO_MASTER.'.date <=', $todate);
         }
-        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number'));        
+        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number',TBL_PACKING_INSTRACTION_DETAILS.'.id'));        
 
         $query = $this->db->get(TBL_BUYER_PO_MASTER_ITEM);
         $rowcount = $query->num_rows();
@@ -10020,7 +10020,7 @@ class Admin_model extends CI_Model
             $this->db->where(TBL_BUYER_PO_MASTER.'.date <=', $todate);
         }
 
-        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number'));        
+        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number',TBL_PACKING_INSTRACTION_DETAILS.'.buyer_invoice_number'));        
         //$this->db->group_by(TBL_BUYER_PO_MASTER.'.buyer_po_part_delivery_date');
         $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
 
