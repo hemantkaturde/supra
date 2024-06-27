bea25b7e8922618e8edf3101ccb69db40da9756f
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-27 21:01:07 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9978,6 +9978,8 @@ class Admin_model extends CI_Model
 
 
     public function fetchbuyerpodetailsreportData($params,$buyer_name,$part_number,$from_date,$to_date){
+        $this->db->distinct();
+
         $this->db->select(TBL_BUYER_PO_MASTER.'.buyer_po_date,'.TBL_BUYER_PO_MASTER.'.sales_order_number,'.TBL_BUYER_MASTER.'.buyer_name,'.TBL_BUYER_PO_MASTER.'.buyer_po_number,'.TBL_BUYER_PO_MASTER.'.date,'.TBL_FINISHED_GOODS.'.part_number,'.TBL_FINISHED_GOODS.'.name,'.TBL_BUYER_PO_MASTER_ITEM.'.order_oty,'.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_part_delivery_date,'.TBL_BUYER_PO_MASTER.'.id as buyer_po_idpo,'.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id as part_number_id_buyer_Po,'.TBL_PACKING_INSTRACTION_DETAILS.'.buyer_invoice_number');
         $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_BUYER_PO_MASTER_ITEM.'.part_number_id = '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number','left');
         $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
@@ -10022,7 +10024,7 @@ class Admin_model extends CI_Model
             $this->db->where(TBL_BUYER_PO_MASTER.'.date <=', $todate);
         }
 
-        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number'));        
+       // $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number'));        
         //$this->db->group_by(TBL_PACKING_INSTRACTION_DETAILS.'.id');
         $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
 
