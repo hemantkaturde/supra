d76c7ba65d3636ce8b97369792cb3a4dfc86190e
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-27 17:32:34 +0530

    code for adede new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9987,6 +9987,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
         $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
         $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_BUYER_PO_MASTER_ITEM.'.part_number_id = '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number','left');
+        $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
 
 
         // if($params['search']['value'] != "") 
