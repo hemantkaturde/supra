035ef767dd935e00654bf1622dda608d5574477d
Hemant katurde <hemantkaturde123@gmail.com>
2024-06-27 21:17:29 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -10023,8 +10023,8 @@ class Admin_model extends CI_Model
             $this->db->where(TBL_BUYER_PO_MASTER.'.date <=', $todate);
         }
 
-        $this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id'));        
-       // $this->db->group_by(TBL_BUYER_PO_MASTER.'.buyer_po_number');
+        //$this->db->group_by(array(TBL_PACKING_INSTRACTION_DETAILS.'.buyer_item_delivery_date',TBL_BUYER_PO_MASTER_ITEM.'.part_number_id',TBL_BUYER_PO_MASTER.'.sales_order_number'));        
+        //$this->db->group_by(TBL_PACKING_INSTRACTION_DETAILS.'.id');
         $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
 
         $this->db->limit($params['length'],$params['start']);
