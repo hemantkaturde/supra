f3822d10904261741043f54f991b11f1dc26042c
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-13 18:53:34 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -2480,30 +2480,39 @@ class Admin_model extends CI_Model
 
         if($flag=='Supplier'){
 
+            // $this->db->select('*,'.TBL_VENDOR_PO_MASTER_ITEM.'.order_oty as vendor_order_qty,'.TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.order_oty as supplier_order_qty,'.TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.sent_qty as supplier_sent_qty');
+            // $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
+            // $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
+            // $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.pre_vendor_name');
+            // $this->db->join(TBL_SUPPLIER_PO_CONFIRMATION_ITEM, TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
+    
+            // $this->db->where(TBL_FINISHED_GOODS.'.status',1);
+            // $this->db->where(TBL_FINISHED_GOODS.'.fin_id',$part_number);
+            // //$this->db->where(TBL_RAWMATERIAL.'.raw_id',$part_number);
+            // $query = $this->db->get(TBL_FINISHED_GOODS);
+            // $data = $query->result_array();
+            // return $data;
+
             $this->db->select('*,'.TBL_VENDOR_PO_MASTER_ITEM.'.order_oty as vendor_order_qty,'.TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.order_oty as supplier_order_qty,'.TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.sent_qty as supplier_sent_qty');
             $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
-            $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
+            $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
             $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.pre_vendor_name');
             $this->db->join(TBL_SUPPLIER_PO_CONFIRMATION_ITEM, TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.part_number_id = '.TBL_RAWMATERIAL.'.raw_id');
-    
             $this->db->where(TBL_FINISHED_GOODS.'.status',1);
             $this->db->where(TBL_FINISHED_GOODS.'.fin_id',$part_number);
-            //$this->db->where(TBL_RAWMATERIAL.'.raw_id',$part_number);
             $this->db->where(TBL_VENDOR_PO_MASTER_ITEM.'.id',$poitemid);
-            $query = $this->db->get(TBL_FINISHED_GOODS);
+            $query = $this->db->get(TBL_VENDOR_PO_MASTER_ITEM);
             $data = $query->result_array();
-            $abc = $this->db->last_query();
-
-            print_r($abc);
-            exit;
             return $data;
+
+
 
         }else{
             $this->db->select('*,'.TBL_VENDOR_PO_MASTER_ITEM.'.order_oty as vendor_order_qty,0 as supplier_order_qty,0 as supplier_sent_qty');
             // $this->db->join(TBL_RAWMATERIAL, TBL_RAWMATERIAL.'.part_number = '.TBL_FINISHED_GOODS.'.part_number');
             $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id = '.TBL_FINISHED_GOODS.'.fin_id');
             $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.pre_vendor_name');
-            $this->db->join(TBL_SUPPLIER_PO_CONFIRMATION_ITEM, TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.part_number_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
+            // $this->db->join(TBL_SUPPLIER_PO_CONFIRMATION_ITEM, TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.vendor_id = '.TBL_VENDOR.'.ven_id');
     
             $this->db->where(TBL_FINISHED_GOODS.'.status',1);
             $this->db->where(TBL_VENDOR_PO_MASTER_ITEM.'.vendor_po_id',$vendor_po_number);
@@ -2511,11 +2520,6 @@ class Admin_model extends CI_Model
             $this->db->where(TBL_FINISHED_GOODS.'.fin_id',$part_number);
             $query = $this->db->get(TBL_FINISHED_GOODS);
             $data = $query->result_array();
-
-            $abc = $this->db->last_query();
-
-            print_r($abc);
-            exit;
             return $data;
         }
 
