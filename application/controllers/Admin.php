ebf68fdf9e0b803d8f849443f648be1fbeeae7a9
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-25 12:51:57 +0530

    code for added new changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5634,7 +5634,7 @@ class Admin extends BaseController
                             $balence_qty = trim($this->input->post('p_o_qty'))-trim($this->input->post('invoice_qty'));
                         }else{
 
-                            $get_previous_balenace_qty = $this->admin_model->get_previous_item_balenace_qty_add(trim($this->input->post('part_number')),'');
+                            $get_previous_balenace_qty = $this->admin_model->get_previous_item_balenace_qty_add(trim($this->input->post('part_number')),'NA');
 
                             if($get_previous_balenace_qty){
                                 $balence_qty = $get_previous_balenace_qty[0]['balance_qty']-trim($this->input->post('invoice_qty'));
