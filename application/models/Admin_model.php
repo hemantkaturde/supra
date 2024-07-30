c0a76d0b72e1bf04d3e2871de70afe77210f6158
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-30 09:55:34 +0530

    code for added brocire link

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -15119,7 +15119,10 @@ public function updatestockaftercalculation($balence_qty_in_pcs,$finishgood_id,$
 public function getpreviousstock($part_number_id,$stock_id){
     $this->db->select(TBL_STOCKS.'.previous_stock');
     $this->db->join(TBL_STOCKS_ITEM, TBL_STOCKS_ITEM.'.stock_form_id = '.TBL_STOCKS.'.stock_id');
-    $this->db->where(TBL_STOCKS.'.stock_id',$stock_id);
+    // $this->db->where(TBL_STOCKS.'.stock_id',$stock_id);
+    $this->db->order_by(TBL_STOCKS_ITEM.'.id','DESC');
+    $this->db->limit(1);
+
     $query = $this->db->get(TBL_STOCKS);
     $fetch_result = $query->row_array();
     return $fetch_result;
