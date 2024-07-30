bfe1db99c9f89258d82b9bbdfb058fd3928245d9
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-30 10:02:34 +0530

    code for added brocire link

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -15122,7 +15122,7 @@ public function getpreviousstock($part_number_id,$stock_id){
     // $this->db->where(TBL_STOCKS.'.stock_id',$stock_id);
  
     // $this->db->order_by(TBL_STOCKS.'.stock_id','DESC');
-    // $this->db->limit(1);
+    $this->db->limit(1,1);
     $query = $this->db->get(TBL_STOCKS);
     $fetch_result = $query->row_array();
     return $fetch_result;
