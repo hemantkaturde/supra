12b6a2104d83fdc2778fd316397ff40964a0066a
Hemant katurde <hemantkaturde123@gmail.com>
2024-07-31 20:14:43 +0530

    code for added brocire link

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -2855,7 +2855,6 @@ class Admin_model extends CI_Model
         $this->db->where(TBL_VENDOR_PO_MASTER.'.id',$vendor_po_number);
         $query = $this->db->get(TBL_VENDOR_PO_MASTER);
         $data = $query->result_array();
-        $str = $this->db->last_query();
         return $data;
 
 
