bf24f51c3bb41752e9744c6a9ec49c690433a88e
Hemant katurde <hemantkaturde123@gmail.com>
2024-08-02 15:52:29 +0530

    code for added new changes

--- application/views/masters/editsuppliervendorcompalint.php
+++ application/views/masters/editsuppliervendorcompalint.php
@@ -53,7 +53,7 @@
                                                   <option value="NA">Select Stage</option>
                                                   <option value="Incoming" <?php if($getsuppliervendorcomplaintdata['stage']=='Incoming'){ echo 'selected'; } ?>>Incoming</option>
                                                   <option value="Inprogress" <?php if($getsuppliervendorcomplaintdata['stage']=='Inprogress'){ echo 'selected'; } ?>>Inprogress</option>
-                                                  <option value="Final Inspection" <?php if($getsuppliervendorcomplaintdata['stage']=='Final Inspection'){ echo 'selected'; } ?>>Find Inspection</option>
+                                                  <option value="Final Inspection" <?php if($getsuppliervendorcomplaintdata['stage']=='Final Inspection'){ echo 'selected'; } ?>>Final Inspection</option>
                                                   <option value="At Vendor End" <?php if($getsuppliervendorcomplaintdata['stage']=='At Vendor End'){ echo 'selected'; } ?>>At Vendor End</option>
                                                   <option value="At Supplier End" <?php if($getsuppliervendorcomplaintdata['stage']=='At Supplier End'){ echo 'selected'; } ?>>At Supplier End</option>
                                             </select>
