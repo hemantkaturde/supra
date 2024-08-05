ebd753c5b5e45c5a623433c3bace9cdbec0fc17a
Hemant katurde <hemantkaturde123@gmail.com>
2024-08-05 10:22:17 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -7670,7 +7670,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.id = '.TBL_REJECTION_FORM_REJECTED_ITEM.'.item_id');
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
         $this->db->join(TBL_REJECTION_FORM, TBL_REJECTION_FORM.'.id = '.TBL_REJECTION_FORM_REJECTED_ITEM.'.rejection_form_id');
-        $this->db->where(TBL_REJECTION_FORM_REJECTED_ITEM.'.item_id',$part_number);
+        //$this->db->where(TBL_REJECTION_FORM_REJECTED_ITEM.'.item_id',$part_number);
         // $this->db->where(TBL_REJECTION_FORM_REJECTED_ITEM.'.vendor_po_id',$vendor_po_id);
         $this->db->where(TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id',$part_number);
 
