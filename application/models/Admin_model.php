9842a793b36bd1c61b78957bda1677089119b574
Hemant katurde <hemantkaturde123@gmail.com>
2024-08-07 14:22:07 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -13557,7 +13557,6 @@ public function fetchproductionstatusreportdata($params,$vendor_name,$status,$pa
 
 
     $this->db->select('*,'.TBL_VENDOR.'.vendor_name as vendorname,'.TBL_BILL_OF_MATERIAL_VENDOR.'.id as billofmaterialid,'.TBL_FINISHED_GOODS.'.part_number as partno,'.TBL_BUYER_MASTER.'.buyer_name as buyer, 2 as flag,'.TBL_BILL_OF_MATERIAL_VENDOR.'.bom_status,'.TBL_VENDOR_PO_MASTER_ITEM.'.order_oty as vendor_order_qty_co,'.TBL_BILL_OF_MATERIAL_VENDOR_ITEM.'.vendor_received_qty as vendor_received_qty_co,'.TBL_VENDOR_PO_MASTER.'.po_number as v_po_number,'.TBL_FINISHED_GOODS.'.name as part_description,'.TBL_VENDOR_PO_MASTER.'.delivery_date,'.TBL_BILL_OF_MATERIAL_VENDOR_ITEM.'.id as vendor_bill_item_id,"Vendor Bill Of Material" as flag,"vbom" as notes_status,'.TBL_BILL_OF_MATERIAL_VENDOR_ITEM.'.notes');
-
     $this->db->join(TBL_BILL_OF_MATERIAL_VENDOR, TBL_BILL_OF_MATERIAL_VENDOR.'.id= '.TBL_BILL_OF_MATERIAL_VENDOR_ITEM.'.vendor_bill_of_material_id');
     $this->db->join(TBL_VENDOR_PO_MASTER, TBL_VENDOR_PO_MASTER.'.id= '.TBL_BILL_OF_MATERIAL_VENDOR.'.vendor_po_number');
     $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id= '.TBL_BILL_OF_MATERIAL_VENDOR_ITEM.'.part_number_id');
@@ -13640,6 +13639,8 @@ public function fetchproductionstatusreportdata($params,$vendor_name,$status,$pa
     $this->db->limit($params['length'],$params['start']);
     
     $this->db->order_by(TBL_BILL_OF_MATERIAL.'.id','DESC');
+    $this->db->group_by(TBL_VENDOR_PO_MASTER_ITEM.'.id');
+
     $query = $this->db->get(TBL_BILL_OF_MATERIAL);
     //$fetch_result = $query->result_array();
     $query2 = $query->result_array();
