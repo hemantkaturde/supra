d0dee8549bac45b2723f950903ec67a1ea0ceefa
Hemant katurde <hemantkaturde123@gmail.com>
2024-08-31 14:37:05 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -11164,7 +11164,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
         $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
-        $this->db->where(TBL_FINISHED_GOODS.'.part_number=',923);
+        $this->db->where(TBL_FINISHED_GOODS.'.fin_id=',923);
 
         if($params['search']['value'] != "") 
         {
