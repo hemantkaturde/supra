8b03b8c69e230f24108bcdc15ce5762a35b5d97c
Hemant katurde <hemantkaturde123@gmail.com>
2024-08-31 15:18:35 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -11185,6 +11185,10 @@ class Admin_model extends CI_Model
 
         $data = array();
         $counter = 0;
+
+        $data_packaing_details = array();
+        $counter_packaing_details = 0;
+
         if(count($fetch_result) > 0)
         {
             foreach ($fetch_result as $key => $value)
@@ -11198,15 +11202,48 @@ class Admin_model extends CI_Model
                     if(count($getPackging_items_for_buyer_po_item) > 0){
 
 
-                 
+                        foreach ($getPackging_items_for_buyer_po_item as $key => $value_2) {
 
 
-                            print_r($getPackging_items_for_buyer_po_item);
+                            if(trim($value['buyer_po_part_delivery_date'])=='0000-00-00'){
+                                $buyer_po_part_delivery_date = '';
+                            }else{
+                                $buyer_po_part_delivery_date = $value['buyer_po_part_delivery_date'];
+                            }
+
+                            $data_packaing_details[$counter_packaing_details]['buyer_name'] =$value['buyer_name'];
+                            $data_packaing_details[$counter_packaing_details]['sales_order_number'] =$value['sales_order_number'].'-'.$value['buyer_po_number'];
+                            $data_packaing_details[$counter_packaing_details]['buyer_po_date'] =$value['buyer_po_date'];
+                            $data_packaing_details[$counter_packaing_details]['part_number'] =$value['part_number'];
+                            $data_packaing_details[$counter_packaing_details]['type_of_raw_material'] =$value['name'];
+                            $data_packaing_details[$counter_packaing_details]['order_qty'] =$value['order_oty'];
+                            $data_packaing_details[$counter_packaing_details]['buyer_po_part_delivery_date'] =$buyer_po_part_delivery_date;
+
+                            $get_export_invoice_details =$this->getexportinvoicedetails($value['buyer_po_idpo'],$value['part_number_id_buyer_Po'],$buyer_po_part_delivery_date,$value_2['buyer_invoice_number']);
+                            if($get_export_invoice_details){
+                                $buyer_invoice_number = $get_export_invoice_details[0]['buyer_invoice_number'];
+                                $buyer_invoice_qty = $get_export_invoice_details[0]['buyer_invoice_qty'];
+                                $buyer_invoice_date = $get_export_invoice_details[0]['buyer_invoice_date'];
+                                $remark = $get_export_invoice_details[0]['remark'];
+                            }else{
+                                $buyer_invoice_number = '';
+                                $buyer_invoice_qty = '';
+                                $buyer_invoice_date = '';
+                                $remark = '';
+                            }
+
+                                $buyer_invoice_number = '';
+                                $buyer_invoice_qty = '';
+                                $buyer_invoice_date = '';
+                                $remark = '';
+
+                            $data_packaing_details[$counter_packaing_details]['export_invoice_number'] = $buyer_invoice_number;
+                            $data_packaing_details[$counter_packaing_details]['buyer_invoice_qty'] =$buyer_invoice_qty;
+                            $data_packaing_details[$counter_packaing_details]['buyer_invoice_date'] =$buyer_invoice_date;
+                            $data_packaing_details[$counter_packaing_details]['remark'] =$remark;
                             
 
-                           
-                         
-
+                        }                 
 
                     }
         
@@ -11214,49 +11251,49 @@ class Admin_model extends CI_Model
                     
                 }else{
                             
-                        // if(trim($value['buyer_po_part_delivery_date'])=='0000-00-00'){
-                        //     $buyer_po_part_delivery_date = '';
-                        // }else{
-                        //     $buyer_po_part_delivery_date = $value['buyer_po_part_delivery_date'];
-                        // }
+                        if(trim($value['buyer_po_part_delivery_date'])=='0000-00-00'){
+                            $buyer_po_part_delivery_date = '';
+                        }else{
+                            $buyer_po_part_delivery_date = $value['buyer_po_part_delivery_date'];
+                        }
 
-                        // $data[$counter]['buyer_name'] =$value['buyer_name'];
-                        // $data[$counter]['sales_order_number'] =$value['sales_order_number'].'-'.$value['buyer_po_number'];
-                        // $data[$counter]['buyer_po_date'] =$value['buyer_po_date'];
-                        // $data[$counter]['part_number'] =$value['part_number'];
-                        // $data[$counter]['type_of_raw_material'] =$value['name'];
-                        // $data[$counter]['order_qty'] =$value['order_oty'];
-                        // $data[$counter]['buyer_po_part_delivery_date'] =$buyer_po_part_delivery_date;
+                        $data[$counter]['buyer_name'] =$value['buyer_name'];
+                        $data[$counter]['sales_order_number'] =$value['sales_order_number'].'-'.$value['buyer_po_number'];
+                        $data[$counter]['buyer_po_date'] =$value['buyer_po_date'];
+                        $data[$counter]['part_number'] =$value['part_number'];
+                        $data[$counter]['type_of_raw_material'] =$value['name'];
+                        $data[$counter]['order_qty'] =$value['order_oty'];
+                        $data[$counter]['buyer_po_part_delivery_date'] =$buyer_po_part_delivery_date;
 
-                        // $get_export_invoice_details =$this->getexportinvoicedetails($value['buyer_po_idpo'],$value['part_number_id_buyer_Po'],$buyer_po_part_delivery_date,$value['buyer_invoice_number']);
-                        // if($get_export_invoice_details){
-                        //     $buyer_invoice_number = $get_export_invoice_details[0]['buyer_invoice_number'];
-                        //     $buyer_invoice_qty = $get_export_invoice_details[0]['buyer_invoice_qty'];
-                        //     $buyer_invoice_date = $get_export_invoice_details[0]['buyer_invoice_date'];
-                        //     $remark = $get_export_invoice_details[0]['remark'];
-                        // }else{
-                        //     $buyer_invoice_number = '';
-                        //     $buyer_invoice_qty = '';
-                        //     $buyer_invoice_date = '';
-                        //     $remark = '';
-                        // }
+                        $get_export_invoice_details =$this->getexportinvoicedetails($value['buyer_po_idpo'],$value['part_number_id_buyer_Po'],$buyer_po_part_delivery_date,$value['buyer_invoice_number']);
+                        if($get_export_invoice_details){
+                            $buyer_invoice_number = $get_export_invoice_details[0]['buyer_invoice_number'];
+                            $buyer_invoice_qty = $get_export_invoice_details[0]['buyer_invoice_qty'];
+                            $buyer_invoice_date = $get_export_invoice_details[0]['buyer_invoice_date'];
+                            $remark = $get_export_invoice_details[0]['remark'];
+                        }else{
+                            $buyer_invoice_number = '';
+                            $buyer_invoice_qty = '';
+                            $buyer_invoice_date = '';
+                            $remark = '';
+                        }
 
-                        //     $buyer_invoice_number = '';
-                        //     $buyer_invoice_qty = '';
-                        //     $buyer_invoice_date = '';
-                        //     $remark = '';
+                            $buyer_invoice_number = '';
+                            $buyer_invoice_qty = '';
+                            $buyer_invoice_date = '';
+                            $remark = '';
 
-                        // $data[$counter]['export_invoice_number'] = $buyer_invoice_number;
-                        // $data[$counter]['buyer_invoice_qty'] =$buyer_invoice_qty;
-                        // $data[$counter]['buyer_invoice_date'] =$buyer_invoice_date;
-                        // $data[$counter]['remark'] =$remark;
+                        $data[$counter]['export_invoice_number'] = $buyer_invoice_number;
+                        $data[$counter]['buyer_invoice_qty'] =$buyer_invoice_qty;
+                        $data[$counter]['buyer_invoice_date'] =$buyer_invoice_date;
+                        $data[$counter]['remark'] =$remark;
                 }
 
 
                 $counter++; 
             }
         }
-        return $data;
+        return $data_packaing_details;
     }
 
 
