eed338fd38874bd5fa069e6c533b3d4718b88260
Hemant katurde <hemantkaturde123@gmail.com>
2024-09-16 14:29:51 +0530

    code for added new changes

--- application/views/masters/buyerpo.php
+++ application/views/masters/buyerpo.php
@@ -16,7 +16,7 @@
             </div>
             <div class="col-xs-6 text-right">
                 <div class="form-group">
-                    <?php if($this->session->userdata('roleText')=='Superadmin' || $this->session->userdata('roleText')=='Sales' ){ ?>
+                    <?php if($this->session->userdata('roleText')=='Superadmin' || $this->session->userdata('roleText')=='Purchase' ){ ?>
                         <a class="btn btn-primary" href="<?php echo base_url(); ?>addnewBuyerpo">
                             <i class="fa fa-plus"></i> Add Buyer PO</a>
                     <?php } ?>
