259480b753745e9e54afb5a7369daf1ecc81e9e5
Hemant katurde <hemantkaturde123@gmail.com>
2024-09-25 10:53:51 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -17597,7 +17597,7 @@ public function updatestockaftercalculation($balence_qty_in_pcs,$finishgood_id,$
                     $data[$counter]['action'] .= "<i style='font-size: x-large;cursor: pointer;' data-id='".$value['usp_incoming_id']."' class='fa fa-trash-o deleteuspincoming' aria-hidden='true'></i>"; 
                 }else{
 
-                    if($this->session->userdata('roleText')==' Stock'){
+                    if($this->session->userdata('roleText')=='Stock'){
                         $data[$counter]['action'] .= "<a href='".ADMIN_PATH."viewuspincomig/".$value['usp_incoming_id']."' style='cursor: pointer;' target='_blank'><i style='font-size: x-large;cursor: pointer;' class='fa fa-file-text-o' aria-hidden='true'></i></a>   &nbsp ";
                         $data[$counter]['action'] .= "<a href='".ADMIN_PATH."edituspincomig/".$value['usp_incoming_id']."' style='cursor: pointer;' target='_blank'><i style='font-size: x-large;cursor: pointer;' class='fa fa-pencil-square-o' aria-hidden='true'></i></a>   &nbsp ";
                         $data[$counter]['action'] .= "<i style='font-size: x-large;cursor: pointer;' data-id='".$value['usp_incoming_id']."' class='fa fa-trash-o deleteuspincoming' aria-hidden='true'></i>"; 
