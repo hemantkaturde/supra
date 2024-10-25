c3cefc423c1e9f5ad93c3ddb13e4ed2a75fdc6b9
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-25 18:16:16 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -19122,7 +19122,7 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
         $this->db->join(TBL_VENDOR_PO_MASTER_ITEM, TBL_VENDOR_PO_MASTER_ITEM.'.vendor_po_id = '.TBL_VENDOR_PO_MASTER.'.id');
         $this->db->join(TBL_VENDOR, TBL_VENDOR.'.ven_id = '.TBL_VENDOR_PO_MASTER.'.vendor_name');
         $this->db->join(TBL_VENDOR_PO_MASTER_ITEM.' as a', 'a.part_number_id = '.TBL_BILL_OF_MATERIAL_ITEM.'.part_number');
-        $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.part_number = '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
+        $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_VENDOR_PO_MASTER_ITEM.'.part_number_id');
 
         
         // $this->db->join(TBL_SUPPLIER_PO_MASTER_ITEM.' as b', 'b.part_number_id = '.TBL_BILL_OF_MATERIAL_ITEM.'.part_number');
