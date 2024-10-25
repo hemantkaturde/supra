984b877d6aa9dd26875a5f75fd9ce1ef92ee8f35
Hemant katurde <hemantkaturde123@gmail.com>
2024-10-25 17:18:33 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -19030,6 +19030,7 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
 
 
         $this->db->order_by(TBL_BILL_OF_MATERIAL_ITEM.'.id','DESC');
+        $this->db->group_by(TBL_BILL_OF_MATERIAL_ITEM.'.id',TBL_SUPPLIER_PO_CONFIRMATION_ITEM.'.part_number_id');
         $query_1 = $this->db->get(TBL_BILL_OF_MATERIAL_ITEM);
         $fetch_result_1 = $query_1->result_array();
 
