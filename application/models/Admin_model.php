2d55425be489ac1f159b134a978e6529707cca2c
Hemant katurde <hemantkaturde123@gmail.com>
2024-12-04 11:24:07 +0530

    code for added new

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -17876,10 +17876,10 @@ public function updatestockaftercalculation($balence_qty_in_pcs,$finishgood_id,$
         $this->db->where(TBL_STOCKS.'.stock_id <',$stock_id);
         $this->db->order_by(TBL_STOCKS.'.stock_id','DESC');
         $query = $this->db->get(TBL_STOCKS);
-        $fetch_result = $query->row_array();
+        $fetch_result_1 = $query->row_array();
 
-        if(count($fetch_result)>1){
-            return $fetch_result;
+        if(count($fetch_result_1)>1){
+            return $fetch_result_1;
         }else{
             $this->db->select(TBL_STOCKS.'.previous_stock');
             // $this->db->join(TBL_STOCKS_ITEM, TBL_STOCKS_ITEM.'.stock_form_id = '.TBL_STOCKS.'.stock_id');
