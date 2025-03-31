a561a66c5736c8d0d7f650975bd75672e87b5f1f
Hemant <hemantkk@riogapremium.com>
2025-04-01 00:23:06 +0530

    code for added new changes

--- application/views/masters/addsbuyerpo.php
+++ application/views/masters/addsbuyerpo.php
@@ -26,12 +26,6 @@
                                 <div class="col-md-4">
                                     <?php
 
-                                        $current_month = date("n"); // Get the current month without leading zeros
-
-                                        $current_date =  date("Y/m/d"); // 
-
-                                        echo $current_month.'-'.$current_date;
-
                                         if ($current_month >= 4) {
                                                 // If the current month is April or later, the financial year is from April (current year) to March (next year)
                                                 $financial_year_indian = date("y") . "" . (date("y") + 1);
