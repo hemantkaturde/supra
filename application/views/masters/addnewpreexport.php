b2a0f9215ceefd878dcb6250325ae23b92ebf6d8
hemant-rioga <hemantkk@riogapremium.com>
2025-03-31 16:08:35 +0530

    code for added Auto incremrnt change

--- application/views/masters/addnewpreexport.php
+++ application/views/masters/addnewpreexport.php
@@ -74,11 +74,11 @@
   
                                               } else {
 
-                                                $getfinancial_year = substr($getPreviousPreexport['pre_export_invoice_no'], -8);
+                                                $getfinancial_year = substr($getPreviousPreexport['pre_export_invoice_no'], -9);
 
                                                 $first_part_of_string = substr($getfinancial_year,0,4);
-
-                                                if($first_part_of_string == $financial_year_indian){
+                                              
+                                                 if($first_part_of_string==$financial_year_indian){
 
                                                     $string = $getPreviousPreexport['pre_export_invoice_no'];
                                                     $n = 4; // Number of characters to extract from the end
@@ -86,7 +86,7 @@
                                                     $inrno= "PEI".$financial_year_indian.'/'.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
                                                     $invoice_number = $inrno;
 
-                                                }else{
+                                                 }else{
 
                                                     $string = $getPreviousPreexport['pre_export_invoice_no'];
                                                     $n = 4; // Number of characters to extract from the end
@@ -109,7 +109,6 @@
                                                     $inrno= "PEI".$financial_year_indian.'/'.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
                                                     $invoice_number = $inrno;
                                                 }
-  
                                                   //$po_number = 'SQPO24250001';
                                               }  
                                             /* New Logic End Here */
