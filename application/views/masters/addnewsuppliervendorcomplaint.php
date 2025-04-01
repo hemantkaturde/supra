0c30d39b6c437bf8e2e5f18a7a10edb629ea3236
hemant-rioga <hemantkk@riogapremium.com>
2025-04-01 14:43:18 +0530

    code for added new changes

--- application/views/masters/addnewsuppliervendorcomplaint.php
+++ application/views/masters/addnewsuppliervendorcomplaint.php
@@ -34,6 +34,18 @@
                                 <div class="row">
 
                                 <?php
+                                        $current_month = date("n"); // Get the current month without leading zeros
+
+                                        if ($current_month >= 4) {
+                                            // If the current month is April or later, the financial year is from April (current year) to March (next year)
+                                            $financial_year_indian = date("y") . "-" . (date("y") + 1);
+                                        } else {
+                                            // If the current month is before April, the financial year is from April (last year) to March (current year)
+                                            $financial_year_indian = (date("y") - 1) . "-" . date("y");
+                                        }
+
+                                
+
                                         if($getPrevioussuppliercustomerCompalinformnumber['report_number']){
                                                 // $arr = str_split($getPreviousSalesOrderNumber['sales_order_number']);
                                                 // $i = end($arr);
@@ -61,13 +73,25 @@
                                                 $string = $getPrevioussuppliercustomerCompalinformnumber['report_number'];
 
                                                 $explod = explode("/",$string);
-                                                
-                                                $n = 4; // Number of characters to extract from the end
-                                                $lastNCharacters = substr($explod[1], -$n);
-                                                //$inrno= "SQBO2324".str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                            
+                                                if($explod[2]== $financial_year_indian ){
+                                                    $n = 4; // Number of characters to extract from the end
+                                                    $lastNCharacters = substr($explod[1], -$n);
+                                                    //$inrno= "SQBO2324".str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+    
+                                                    $inrno= 'SV/'.($lastNCharacters+1)."/".$financialYear;
+                                                    $report_no = $inrno;
+                                                }else{
 
-                                                $inrno= 'SV/'.($lastNCharacters+1)."/".$financialYear;
-                                                $report_no = $inrno;
+                                                    $n = 4; // Number of characters to extract from the end
+                                                    $lastNCharacters = 0;
+                                                    //$inrno= "SQBO2324".str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+    
+                                                    $inrno= 'SV/'.($lastNCharacters+1)."/".$financialYear;
+                                                    $report_no = $inrno;
+                                                }
+
+                                              
 
                                         }else{
 
