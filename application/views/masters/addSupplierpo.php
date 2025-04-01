873a5fee07336776ad440db10e789c51af9de81b
hemant-rioga <hemantkk@riogapremium.com>
2025-04-01 15:45:56 +0530

    code for added new change

--- application/views/masters/addSupplierpo.php
+++ application/views/masters/addSupplierpo.php
@@ -292,11 +292,29 @@
                                                           $po_number = $inrno;
 
                                                       } else {
-                                                          $string = $getPreviousvendorPONumber_number;
+
+                                                        $getfinancial_year = substr($getPreviousvendorPONumber['po_number'], -8);
+
+                                                        $first_part_of_string = substr($getfinancial_year,0,4);
+
+                                                        if($first_part_of_string == $financial_year_indian){
+
+
+                                                          $string = $getPreviousvendorPONumber['po_number'];
                                                           $n = 4; // Number of characters to extract from the end
                                                           $lastNCharacters = substr($string, -$n);
                                                           $inrno= "SQPO".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
                                                           $po_number = $inrno;
+                                                        }else{
+
+
+                                                            $string = 0;
+                                                            $n = 4; // Number of characters to extract from the end
+                                                            $lastNCharacters = substr($string, -$n);
+                                                            $inrno= "SQPO".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                            $po_number = $inrno;
+
+                                                        }
 
                                                       }  
                                                     /* New Logic End Here */
