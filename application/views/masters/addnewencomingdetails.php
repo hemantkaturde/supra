17b08d2fae72dcfd5a59632fe40cfdef475a3323
Hemant <hemantkk@riogapremium.com>
2025-04-02 07:13:09 +0530

    code for added new changes

--- application/views/masters/addnewencomingdetails.php
+++ application/views/masters/addnewencomingdetails.php
@@ -73,11 +73,28 @@
 
                                             } else {
 
-                                                $string = $getPreviousincomingdetails[0]['incoming_details_id'];
-                                                $n = 4; // Number of characters to extract from the end
-                                                $lastNCharacters = substr($string, -$n);
-                                                $inrno= "SQID".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
-                                                $incoming_details_id = $inrno;
+                                                
+                                                $getfinancial_year = substr($getPreviousincomingdetails[0]['incoming_details_id'], -8);
+
+                                                $first_part_of_string = substr($getfinancial_year,0,4);
+    
+                                                if($first_part_of_string == $financial_year_indian){
+
+                                                    $string = $getPreviousincomingdetails[0]['incoming_details_id'];
+                                                    $n = 4; // Number of characters to extract from the end
+                                                    $lastNCharacters = substr($string, -$n);
+                                                    $inrno= "SQID".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                    $incoming_details_id = $inrno;
+
+                                                }else{
+                                                    $string =0;
+                                                    $n = 4; // Number of characters to extract from the end
+                                                    $lastNCharacters = substr($string, -$n);
+                                                    $inrno= "SQID".$financial_year_indian.str_pad((int)$lastNCharacters+1, 4, 0, STR_PAD_LEFT);
+                                                    $incoming_details_id = $inrno;
+                                                }
+
+                                             
 
                                                 //$po_number = 'SQPO24250001';
                                             }  
