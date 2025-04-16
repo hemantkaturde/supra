0841d0355d9121f12919c5d7842b31627d5c6068
hemant-rioga <hemantkk@riogapremium.com>
2025-04-16 15:38:31 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -6682,7 +6682,7 @@ class Admin_model extends CI_Model
                     $data[$counter]['action'] .= "<i style='font-size: x-large;cursor: pointer;' data-id='".$value['debit_id']."' class='fa fa-trash-o deletedebitnote' aria-hidden='true'></i>"; 
                 }else{
 
-                    if($this->session->userdata('roleText')=='Purchase' || $this->session->userdata('roleText')=='Stock'){
+                    if($this->session->userdata('roleText')=='Purchase' || $this->session->userdata('roleText')=='Stock'  || $this->session->userdata('roleText')=='Sales'){
 
                         $data[$counter]['action'] .= "<a href='".ADMIN_PATH."viewdebitnoteform/".$value['debit_id']."' style='cursor: pointer;' target='_blank'><i style='font-size: x-large;cursor: pointer;' class='fa fa-file-text-o' aria-hidden='true'></i></a>   &nbsp ";
                         $data[$counter]['action'] .= "<a href='".ADMIN_PATH."editdebitnoteform/".$value['debit_id']."' style='cursor: pointer;' target='_blank'><i style='font-size: x-large;cursor: pointer;' class='fa fa-pencil-square-o' aria-hidden='true'></i></a>   &nbsp ";
