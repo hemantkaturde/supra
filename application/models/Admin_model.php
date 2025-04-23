db03307552f7390fc675ef0c87244f54552cd393
hemant-rioga <hemantkk@riogapremium.com>
2025-04-23 16:23:36 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4510,7 +4510,7 @@ class Admin_model extends CI_Model
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number');
 
         /*New item Details*/
-       // $this->db->join(TBL_BUYER_PO_MASTER_ITEM, TBL_BUYER_PO_MASTER_ITEM.'.part_number_id = '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number');
+        // $this->db->join(TBL_BUYER_PO_MASTER_ITEM, TBL_BUYER_PO_MASTER_ITEM.'.part_number_id = '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number');
 
         $this->db->where(TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id', $main_id);
         $this->db->where(TBL_PACKING_INSTRACTION_DETAILS.'.status', 1);
@@ -21048,10 +21048,12 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
 
     public function getexporthistoryreportdata($params){
 
-        $this->db->select('*');
+        $this->db->select('*,'.TBL_BUYER_MASTER.'.buyer_name as by_name,'.TBL_FINISHED_GOODS.'.part_number as p_name');
         $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id  = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
         $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id  = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
         $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id  = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
+        $this->db->join(TBL_PACKING_INSTRACTION, TBL_PACKING_INSTRACTION.'.buyer_po_number = '.TBL_BUYER_PO_MASTER.'.id');
+        $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id = '.TBL_PACKING_INSTRACTION.'.id  and '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number= '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
         
 
         // if($params['search']['value'] != "") 
@@ -21076,14 +21078,14 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
         {
             foreach ($fetch_result as $key => $value)
             {
-                $data[$counter]['buyer_name'] = $value['buyer_name'];
-                $data[$counter]['part_number'] = $value['part_number'];
+                $data[$counter]['buyer_name'] = $value['by_name'];
+                $data[$counter]['part_number'] = $value['p_name'];
                 $data[$counter]['sales_order_number'] = $value['sales_order_number'];
                 $data[$counter]['date'] = $value['date'];
                 $data[$counter]['order_oty'] = $value['order_oty'];
-                $data[$counter]['total_weight'] = $value['total_weight'];
-                $data[$counter]['total_goni'] = $value['total_goni'];
-                $data[$counter]['qty_in_kgs'] = $value['qty_in_kgs'];
+                $data[$counter]['export_qty'] = $value['buyer_invoice_qty'];
+                $data[$counter]['buyer_invoice_number'] = $value['buyer_invoice_number'];
+                $data[$counter]['buyer_invoice_date'] = $value['buyer_invoice_date'];
                 $data[$counter]['qty_in_kgs1'] ='';
                 $data[$counter]['qty_in_kgs2'] ='';
                 $data[$counter]['qty_in_kgs3'] ='';
