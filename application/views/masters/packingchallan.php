bd9cd7554d52f6626ae036ae9a3ef879ab3ffb57
hemant-rioga <hemantkk@riogapremium.com>
2025-04-28 12:30:11 +0530

    code for added new changes

--- application/views/masters/packingchallan.php
+++ application/views/masters/packingchallan.php
@@ -16,10 +16,10 @@
             </div>
             <div class="col-xs-6 text-right">
                 <div class="form-group">
-                <!-- <?php if($this->session->userdata('roleText')=='Superadmin' || $this->session->userdata('roleText')=='QC'){ ?> -->
+                <?php if($this->session->userdata('roleText')=='Superadmin' || $this->session->userdata('roleText')=='QC'  || $this->session->userdata('roleText')=='Purchase'){ ?>
                     <a class="btn btn-primary" href="<?php echo base_url(); ?>addnewpackingchallan">
                         <i class="fa fa-plus"></i> Add Packing Challan</a>
-                 <!-- <?php } ?> -->
+                 <?php } ?>
                 </div>
             </div>
         </div>
