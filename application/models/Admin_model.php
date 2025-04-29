aa239e0061bae8bb7adafa795ab533b42a9fb765
hemant-rioga <hemantkk@riogapremium.com>
2025-04-29 18:23:51 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -21179,77 +21179,77 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
         return $fetch_result;
     }
 
-    // public function downlaodexporthistoryreportdata($part_number,$buyer_name,$from_date,$to_date){
+    public function downlaodexporthistoryreportdata($part_number,$buyer_name,$from_date,$to_date){
 
-    //     $this->db->select('*,'.TBL_BUYER_MASTER.'.buyer_name as by_name,'.TBL_FINISHED_GOODS.'.part_number as p_name');
-    //     $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id  = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
-    //     $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id  = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
-    //     $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id  = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
-    //     $this->db->join(TBL_PACKING_INSTRACTION, TBL_PACKING_INSTRACTION.'.buyer_po_number = '.TBL_BUYER_PO_MASTER.'.id');
-    //     $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id = '.TBL_PACKING_INSTRACTION.'.id  and '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number= '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
-    //     $this->db->join(TBL_PREEXPORT, TBL_PREEXPORT.'.buyer_po = '.TBL_BUYER_PO_MASTER.'.id');
-    //     $this->db->join(TBL_PREEXPORT_ITEM_DETAILS, TBL_PREEXPORT_ITEM_DETAILS.'.pre_export_id = '.TBL_PREEXPORT.'.id  and '.TBL_PREEXPORT_ITEM_DETAILS.'.part_number= '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
-
-
-    //     // if($params['search']['value'] != "") 
-    //     // {
-    //     //     $this->db->where("(".TBL_PACKING_CHALLAN.".packing_challan_id LIKE '%".$params['search']['value']."%'");
-    //     //     $this->db->or_where(TBL_PACKING_CHALLAN.".packing_challan_date LIKE '%".$params['search']['value']."%'");
-    //     //     $this->db->or_where(TBL_PACKING_CHALLAN.".total_goni LIKE '%".$params['search']['value']."%'");
-    //     //     $this->db->or_where(TBL_PACKING_CHALLAN.".dispatched_by LIKE '%".$params['search']['value']."%'");
-    //     //     $this->db->or_where(TBL_VENDOR.".vendor_name LIKE '%".$params['search']['value']."%'");
-    //     //     $this->db->or_where(TBL_PACKING_CHALLAN.".total_weight LIKE '%".$params['search']['value']."%')");
-    //     // }
-
-    //     if($part_number!='NA'){
-    //         $this->db->where(TBL_FINISHED_GOODS.'.fin_id', $part_number);
-    //     }     
+        $this->db->select('*,'.TBL_BUYER_MASTER.'.buyer_name as by_name,'.TBL_FINISHED_GOODS.'.part_number as p_name');
+        $this->db->join(TBL_BUYER_PO_MASTER, TBL_BUYER_PO_MASTER.'.id  = '.TBL_BUYER_PO_MASTER_ITEM.'.buyer_po_id');
+        $this->db->join(TBL_BUYER_MASTER, TBL_BUYER_MASTER.'.buyer_id  = '.TBL_BUYER_PO_MASTER.'.buyer_name_id');
+        $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id  = '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
+        $this->db->join(TBL_PACKING_INSTRACTION, TBL_PACKING_INSTRACTION.'.buyer_po_number = '.TBL_BUYER_PO_MASTER.'.id');
+        $this->db->join(TBL_PACKING_INSTRACTION_DETAILS, TBL_PACKING_INSTRACTION_DETAILS.'.packing_instract_id = '.TBL_PACKING_INSTRACTION.'.id  and '.TBL_PACKING_INSTRACTION_DETAILS.'.part_number= '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
+        $this->db->join(TBL_PREEXPORT, TBL_PREEXPORT.'.buyer_po = '.TBL_BUYER_PO_MASTER.'.id');
+        $this->db->join(TBL_PREEXPORT_ITEM_DETAILS, TBL_PREEXPORT_ITEM_DETAILS.'.pre_export_id = '.TBL_PREEXPORT.'.id  and '.TBL_PREEXPORT_ITEM_DETAILS.'.part_number= '.TBL_BUYER_PO_MASTER_ITEM.'.part_number_id');
 
 
-    //     if($buyer_name!='NA'){
-    //         $this->db->where(TBL_BUYER_MASTER.'.buyer_id', $buyer_name);
-    //     }        
+        // if($params['search']['value'] != "") 
+        // {
+        //     $this->db->where("(".TBL_PACKING_CHALLAN.".packing_challan_id LIKE '%".$params['search']['value']."%'");
+        //     $this->db->or_where(TBL_PACKING_CHALLAN.".packing_challan_date LIKE '%".$params['search']['value']."%'");
+        //     $this->db->or_where(TBL_PACKING_CHALLAN.".total_goni LIKE '%".$params['search']['value']."%'");
+        //     $this->db->or_where(TBL_PACKING_CHALLAN.".dispatched_by LIKE '%".$params['search']['value']."%'");
+        //     $this->db->or_where(TBL_VENDOR.".vendor_name LIKE '%".$params['search']['value']."%'");
+        //     $this->db->or_where(TBL_PACKING_CHALLAN.".total_weight LIKE '%".$params['search']['value']."%')");
+        // }
+
+        if($part_number!='NA'){
+            $this->db->where(TBL_FINISHED_GOODS.'.fin_id', $part_number);
+        }     
+
+
+        if($buyer_name!='NA'){
+            $this->db->where(TBL_BUYER_MASTER.'.buyer_id', $buyer_name);
+        }        
     
-    //     if($from_date!='NA'){
-    //         $this->db->where(TBL_BUYER_PO_MASTER.".date >=", $from_date);
-    //     }
+        if($from_date!='NA'){
+            $this->db->where(TBL_BUYER_PO_MASTER.".date >=", $from_date);
+        }
     
-    //     if($to_date!='NA'){
-    //         $this->db->where(TBL_BUYER_PO_MASTER.".date <=", $to_date);
-    //     }
+        if($to_date!='NA'){
+            $this->db->where(TBL_BUYER_PO_MASTER.".date <=", $to_date);
+        }
 
 
-    //     // $this->db->where(TBL_PACKING_CHALLAN.'.status', 1);
-    //     $this->db->limit($params['length'],$params['start']);
-    //     $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
-    //     $query = $this->db->get(TBL_BUYER_PO_MASTER_ITEM);
-    //     $fetch_result = $query->result_array();
+        // $this->db->where(TBL_PACKING_CHALLAN.'.status', 1);
+        $this->db->limit($params['length'],$params['start']);
+        $this->db->order_by(TBL_BUYER_PO_MASTER_ITEM.'.id','DESC');
+        $query = $this->db->get(TBL_BUYER_PO_MASTER_ITEM);
+        $fetch_result = $query->result_array();
 
-    //     $data = array();
-    //     $counter = 0;
-    //     if(count($fetch_result) > 0)
-    //     {
-    //         foreach ($fetch_result as $key => $value)
-    //         {
-    //             $data[$counter]['buyer_name'] = $value['by_name'];
-    //             $data[$counter]['part_number'] = $value['p_name'];
-    //             $data[$counter]['sales_order_number'] = $value['sales_order_number'];
-    //             $data[$counter]['date'] = $value['date'];
-    //             $data[$counter]['order_oty'] = $value['order_oty'];
-    //             $data[$counter]['export_qty'] = $value['buyer_invoice_qty'];
-    //             $data[$counter]['buyer_invoice_number'] = $value['buyer_invoice_number'];
-    //             $data[$counter]['buyer_invoice_date'] = $value['buyer_invoice_date'];
-    //             $data[$counter]['mode_of_shipment'] =$value['mode_of_shipment'];
-    //             $data[$counter]['current_stock'] =$value['current_stock'];
-    //             $data[$counter]['qty_in_kgs3'] =$value['order_oty']- $value['buyer_invoice_qty'];
+        $data = array();
+        $counter = 0;
+        if(count($fetch_result) > 0)
+        {
+            foreach ($fetch_result as $key => $value)
+            {
+                $data[$counter]['buyer_name'] = $value['by_name'];
+                $data[$counter]['part_number'] = $value['p_name'];
+                $data[$counter]['sales_order_number'] = $value['sales_order_number'];
+                $data[$counter]['date'] = $value['date'];
+                $data[$counter]['order_oty'] = $value['order_oty'];
+                $data[$counter]['export_qty'] = $value['buyer_invoice_qty'];
+                $data[$counter]['buyer_invoice_number'] = $value['buyer_invoice_number'];
+                $data[$counter]['buyer_invoice_date'] = $value['buyer_invoice_date'];
+                $data[$counter]['mode_of_shipment'] =$value['mode_of_shipment'];
+                $data[$counter]['current_stock'] =$value['current_stock'];
+                $data[$counter]['qty_in_kgs3'] =$value['order_oty']- $value['buyer_invoice_qty'];
 
-    //             $counter++; 
-    //         }
-    //     }
+                $counter++; 
+            }
+        }
 
-    //     return $data;
+        return $data;
 
-    // }
+    }
 
 }
 
