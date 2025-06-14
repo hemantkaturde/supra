0ff7162346f052ae18a270fb97fad073f4c59df2
Hemant <hemantkk@riogapremium.com>
2025-06-14 23:02:29 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -21318,12 +21318,19 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
        
         $this->db->where(TBL_PACKING_INSTRACTION_DETAILS.".id <", $packgaing_instructin_details_id);
         // $this->db->where(TBL_PACKING_INSTRACTION_DETAILS .".packing_instract_id", $packgaing_instructin_id);
-        //$this->db->group_by(TBL_PACKING_INSTRACTION_DETAILS.'.id');
+        $this->db->group_by(TBL_PACKING_INSTRACTION_DETAILS.'.id');
         $this->db->order_by(TBL_PACKING_INSTRACTION_DETAILS.'.id', 'DESC');
         //$this->db->limit(1);
         $query = $this->db->get();
         $fetch_result = $query->result_array();
   
+
+        print_r($this->db->last_query());
+        exit;
+
+
+
+
 
         $data = array();
         $counter = 0;
