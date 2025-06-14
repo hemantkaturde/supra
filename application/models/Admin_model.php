3c554f9a0b3fde3e8661115ee34d5bed1d2e5847
Hemant <hemantkk@riogapremium.com>
2025-06-14 19:14:14 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -21261,10 +21261,9 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
                 if($previous_stock == 0){
                    $bal_qty = $value['order_oty']-$value['buyer_invoice_qty'];
                 }else{
-                   $bal_qty = $previous_stock -$getPreviousbaldependsonpackgingid[0]['buyer_invoice_qty'];
+                   $bal_qty = $previous_stock -$value['buyer_invoice_qty'];
                 }
 
-                
                 $data[$counter]['buyer_name'] = $value['by_name'];
                 $data[$counter]['part_number'] = $value['p_name'];
                 $data[$counter]['sales_order_number'] = $value['sales_order_number'].'-'.$value['original'];
