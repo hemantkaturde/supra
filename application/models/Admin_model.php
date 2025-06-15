28878fb051fc5ff075bf68728e614f25b3ba57e7
Hemant <hemantkk@riogapremium.com>
2025-06-15 13:04:23 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -21330,11 +21330,6 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
         $query = $this->db->get();
         $fetch_result = $query->result_array();
 
-        print_r($this->db->last_query());
-        exit;
-
-
-
         $data = array();
         $counter = 0;
         if(count($fetch_result) > 0)
