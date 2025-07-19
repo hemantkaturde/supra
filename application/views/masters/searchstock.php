9679e6f4cfb4baee5c2d55fddd3c210dfba6285d
Hemant <hemantkk@riogapremium.com>
2025-07-19 17:01:58 +0530

    code for added new changes

--- application/views/masters/searchstock.php
+++ application/views/masters/searchstock.php
@@ -130,7 +130,7 @@
 
              <div class="col-md-3">
                 <div class="form-group">
-                    <label for="row_material_rm_type">Supplier PO </label>
+                    <label for="row_material_rm_type">RM Type</label>
                     <input type="text" class="form-control" id="row_material_rm_type" name="row_material_rm_type" value="<?=$getsearchstockvendordeatils[0]['row_material_rm_type'];?>" style="background: #FFF;" readonly>
                 </div>
             </div>
