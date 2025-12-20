4353e51392c50d17a33fa596d5b5d91d93e4006f
hemant-rioga <hemantkk@riogapremium.com>
2025-12-20 16:52:48 +0530

    code for aaded new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -22693,7 +22693,7 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
             $this->db->join(TBL_FINISHED_GOODS, TBL_FINISHED_GOODS.'.fin_id = '.TBL_REWORK_RECORD.'.part_no');
             $this->db->join(TBL_TEAM_MASTER, TBL_TEAM_MASTER.'.id = '.TBL_REWORK_RECORD.'.team','left');
             $this->db->where(TBL_REWORK_RECORD.'.id', $id);
-            $this->db->where(TBL_INCOMING_DETAILS_ITEM.'.id', $rjection_incoming_item_id);
+            // $this->db->where(TBL_INCOMING_DETAILS_ITEM.'.id', $rjection_incoming_item_id);
             // $this->db->where(TBL_REWORK_RECORD.'.id', $id);
             $query = $this->db->get(TBL_REWORK_RECORD);
             $fetch_result = $query->result_array();
