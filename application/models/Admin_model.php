fcbc642997c897ab8b877640deb6fcbee6d01664
hemant-rioga <hemantkk@riogapremium.com>
2026-01-17 15:37:21 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -8524,7 +8524,7 @@ class Admin_model extends CI_Model
                 $data[$counter]['packing_instrauction_id'] =$value['buyer_invoice_number'];
                 $data[$counter]['buyer_invoice_date'] =$value['buyer_invoice_date'];
                 $data[$counter]['buyer_invoice_qty'] =$value['buyer_invoice_qty'];
-                $data[$counter]['export_qty_in_kgs'] = $value['buyer_invoice_qty'] *  $value['net_weight'];
+                $data[$counter]['export_qty_in_kgs'] = round($value['buyer_invoice_qty'] *  $value['net_weight'],3);
                 //$data[$counter]['export_qty_in_kgs'] = '';
                 $data[$counter]['buyer_po_date'] =$value['buyer_invoice_date'];
                 $counter++; 
