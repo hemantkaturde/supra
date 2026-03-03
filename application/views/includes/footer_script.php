deb43f8c0eb0d44881d184ac45deb8cf9006aeb4
hemant-rioga <hemantkk@riogapremium.com>
2026-03-03 19:04:08 +0530

    code for added new changes

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -29442,6 +29442,38 @@ $('#export_excel').on('click', function() {
 				});
 			});
 
+
+			$(document).on("click", ".getinstrumentcertificate", function () {
+
+			var instrument_id  = $(this).data("instrument_id");
+			$.ajax({
+				url : "<?php echo ADMIN_PATH;?>admin/getinstrumentcertificates",
+				type: "POST",
+				data : {'instrument_id' : instrument_id},
+				success: function(data, textStatus, jqXHR)
+					{
+					    $(".loader_ajax").hide();
+							if(data == "failure")
+								{
+									$('#certificate').html('<option value="">Select Part Number</option>');
+												
+								}
+							else
+								{
+									$('#certificate').html(data);
+						    	}
+					},
+				        error: function (jqXHR, textStatus, errorThrown)
+									{
+										$('#certificate').html();
+									}
+						});
+						return false;
+			// $("#instrument_type_dropdown").val(type).trigger("change");
+			// $("#grade_dropdown").val(grade).trigger("change");
+		});
+
+
 	</script>
 <?php } ?>
 
