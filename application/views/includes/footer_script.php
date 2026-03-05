1307e1bd4f2d2b39d87527ec3b0da2a10c792061
hemant-rioga <hemantkk@riogapremium.com>
2026-03-05 07:24:03 +0530

    code for added new

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -29307,7 +29307,7 @@ $('#export_excel').on('click', function() {
 						$('#ticket_no').val(fetchResponse.ticket_no);
 						$('#instrument_name').val(fetchResponse.instrument_name);
 						$('#measuring_size').val(fetchResponse.measuring_size);
-						$('#certificate').val(fetchResponse.certificate);
+						$('#certificate').val(fetchResponse.certificate).trigger('change');
 
 						
 						
