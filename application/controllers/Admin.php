da47dae67fdc04340053ca8244c3b582bbf8a68d
hemant-rioga <hemantkk@riogapremium.com>
2026-03-14 18:35:13 +0530

    code for added new changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -26825,10 +26825,6 @@ public function getvendorpartdetialstdir_report(){
     if($part_no) {
         $part_no_data = $this->admin_model->getvendorpartdetialstdir_report($part_no,$vendor_po_number);
 
-        print_r($part_no_data);
-        exit;
-
-
         if(count($part_no_data) >= 1) {
             echo json_encode($part_no_data[0]);
         } else {
