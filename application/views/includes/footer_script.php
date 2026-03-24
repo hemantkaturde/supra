3fff546d196b8bf4a3ae3ae92c0e3bb1991d3ff9
hemant-rioga <hemantkk@riogapremium.com>
2026-03-24 20:37:28 +0530

    code for added new changes

--- application/views/includes/footer_script.php
+++ application/views/includes/footer_script.php
@@ -29417,14 +29417,16 @@ $('#export_excel').on('click', function() {
 			var class_1 = $('#class_1').val();
 			var part_number = $('#part_number').val();
 
-			var certificate = $('#certificate').val();
+			var certificateremove = $('#certificateremove').val();
 			var status = $('#status').val();
+
+
 
 			
 			$.ajax({
 				url: "<?php echo base_url('editstoreformqtyremovedata'); ?>",
 				type: "POST",
-				data: {"ticket_no_popup": ticket_no_popup,"instrument_name_popup":instrument_name_popup, "measuring_size_popup":measuring_size_popup,"qty_popup":qty_popup, "qty_assign":qty_assign, "qty_remark":qty_remark, "qty_removed": qty_removed,'sampling_id':sampling_id,'sampling_trans_id':sampling_trans_id,"type":type,"grade":grade,"unit":unit,"class_1":class_1,"part_number":part_number,"certificate":certificate,"status":status},
+				data: {"ticket_no_popup": ticket_no_popup,"instrument_name_popup":instrument_name_popup, "measuring_size_popup":measuring_size_popup,"qty_popup":qty_popup, "qty_assign":qty_assign, "qty_remark":qty_remark, "qty_removed": qty_removed,'sampling_id':sampling_id,'sampling_trans_id':sampling_trans_id,"type":type,"grade":grade,"unit":unit,"class_1":class_1,"part_number":part_number,"certificateremove":certificateremove,"status":status},
 				dataType: "json",
                 cache:false,
 				success: function(fetchResponse, textStatus, jqXHR)
