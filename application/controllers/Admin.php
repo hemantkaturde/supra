5120a1dc5c5f8d8bca1e9a7b8c0844f7712cbf01
hemant-rioga <hemantkk@riogapremium.com>
2026-05-22 17:39:04 +0530

    code for added new changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -24617,7 +24617,7 @@ public function emailpaymentdetails(){
             $headers .= "From: 'Supra Quality Exports (I) Pvt. Ltd' <supraportal@supraexports.in>" . "\r\n";  // The "From" field with name and email address
             $headers .= "Reply-To: supraportal@supraexports.in" . "\r\n";  // The reply-to email address
             // $headers .= "Cc: $cc" . "\r\n";  // The CC recipient field
-            $headers .= "CC: kunalsupraexports@gmail.com, purchase2@supraexports.in, purchase1@supraexports.in, purchase@supraexports.in\r\n"; // Multiple CC addresses
+            $headers .= "CC: kunalsupraexports@gmail.com, purchase2@supraexports.in, purchase1@supraexports.in,Purchase3@supraexports.in,purchase@supraexports.in\r\n"; // Multiple CC addresses
 
             $headers .= "X-Mailer: PHP/" . phpversion();  // Info about the PHP version
 
