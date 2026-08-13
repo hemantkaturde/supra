c79e37fb5a3e37a553beacd7a35cb67402ffa874
hemant-rioga <hemantkk@riogapremium.com>
2026-08-13 17:27:15 +0530

    code for added new changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -28451,8 +28451,8 @@ public function checklotnumberisexitsornotadd($usp_incoming_item_id,$lot_no,$pre
       public function getinspectiondataforchecklistreport_data($vendor_po_id,$part_numner_id_og){
 
         $this->db->select(TBL_TDIR.'.id,'.TBL_TDIR.'.report_number,'.TBL_TDIR.'.inspection_report_date');
-        $this->db->where(TBL_TDIR.'.part_number', $part_numner_id_og);
-        $this->db->where(TBL_TDIR.'.vendor_po', $vendor_po_id);
+        // $this->db->where(TBL_TDIR.'.part_number', $part_numner_id_og);
+        // $this->db->where(TBL_TDIR.'.vendor_po', $vendor_po_id);
         $query_result = $this->db->get(TBL_TDIR)->result_array();
         return $query_result;
     }
